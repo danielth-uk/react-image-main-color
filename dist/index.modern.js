0175fdbc0d33428f8e21a4fb47df964f9b333d32
Daniel Thompson <thompson22email@gmail.com>
2022-08-03 13:17:17 +0100

    Version 1.0.4 Dist

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,18 +1,86 @@
 import { useState, useEffect, createElement, useRef } from 'react';
 
-const FileAreaInput = ({
-  container,
-  setColorData,
-  dropActiveClass,
-  imageBaseData
-}) => {
-  const [hexValue, setHexValue] = useState();
-  const [activeDrop, setActiveDrop] = useState(false);
-  useEffect(() => {
+var getAverageRGB = function getAverageRGB(imgBase) {
+  try {
+    var blockSize, defaultRGB, canvas, context, data, width, height, i, length, rgb, count;
+    var dimensions = {
+      w: 0,
+      h: 0
+    };
+    return Promise.resolve(getImageDimensions(imgBase)).then(function (_getImageDimensions) {
+      dimensions = _getImageDimensions;
+      var imgEl = document.createElement('img');
+      imgEl.src = imgBase;
+      blockSize = 5;
+      defaultRGB = {
+        r: 0,
+        g: 0,
+        b: 0
+      };
+      canvas = document.createElement('canvas');
+      context = canvas.getContext && canvas.getContext('2d');
+      i = -4;
+      rgb = {
+        r: 0,
+        g: 0,
+        b: 0
+      };
+      count = 0;
+
+      if (!context) {
+        return defaultRGB;
+      }
+
+      height = canvas.height = dimensions.h;
+      width = canvas.width = dimensions.w;
+      context.drawImage(imgEl, 0, 0);
+
+      try {
+        data = context.getImageData(0, 0, width, height);
+      } catch (e) {
+        console.log(e);
+        alert('x');
+        return defaultRGB;
+      }
+
+      length = data.data.length;
+
+      while ((i += blockSize * 4) < length) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i + 1];
+        rgb.b += data.data[i + 2];
+      }
+
+      rgb.r = ~~(rgb.r / count);
+      rgb.g = ~~(rgb.g / count);
+      rgb.b = ~~(rgb.b / count);
+      return rgb;
+    });
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+
+var FileAreaInput = function FileAreaInput(_ref) {
+  var container = _ref.container,
+      setColorData = _ref.setColorData,
+      dropActiveClass = _ref.dropActiveClass,
+      imageBaseData = _ref.imageBaseData;
+
+  var _React$useState = useState(),
+      hexValue = _React$useState[0],
+      setHexValue = _React$useState[1];
+
+  var _React$useState2 = useState(false),
+      activeDrop = _React$useState2[0],
+      setActiveDrop = _React$useState2[1];
+
+  useEffect(function () {
     setColorData(hexValue);
   }, [hexValue]);
 
-  const handleFile = event => {
+  var handleFile = function handleFile(event) {
     event.preventDefault();
 
     if (imageBaseData !== undefined) {
@@ -32,35 +100,40 @@ const FileAreaInput = ({
       width: "fit-content",
       height: "fit-content"
     },
-    onDragOver: e => {
+    onDragOver: function onDragOver(e) {
       setActiveDrop(true);
       e.preventDefault();
     },
-    onDragLeave: e => {
+    onDragLeave: function onDragLeave(e) {
       setActiveDrop(false);
       e.preventDefault();
     },
-    onDrop: e => handleFile(e)
+    onDrop: function onDrop(e) {
+      return handleFile(e);
+    }
   }, container);
 };
-const FileButtonInput = ({
-  button,
-  setColorData,
-  imageBaseData
-}) => {
-  const [hexValue, setHexValue] = useState();
-  const buttonRef = useRef();
-  useEffect(() => {
+var FileButtonInput = function FileButtonInput(_ref2) {
+  var button = _ref2.button,
+      setColorData = _ref2.setColorData,
+      imageBaseData = _ref2.imageBaseData;
+
+  var _React$useState3 = useState(),
+      hexValue = _React$useState3[0],
+      setHexValue = _React$useState3[1];
+
+  var buttonRef = useRef();
+  useEffect(function () {
     setColorData(hexValue);
   }, [hexValue]);
 
-  const handleClick = () => {
+  var handleClick = function handleClick() {
     if (buttonRef.current !== undefined) {
       buttonRef.current.click();
     }
   };
 
-  const handleFile = event => {
+  var handleFile = function handleFile(event) {
     if (imageBaseData !== undefined) {
       var base64 = getBase64(event.target.files[0]);
 
@@ -90,7 +163,7 @@ function getMainColor(file, setHexValue) {
   var base64 = getBase64(file);
 
   base64.onload = function () {
-    getAverageRGB(base64.result).then(values => {
+    getAverageRGB(base64.result).then(function (values) {
       setHexValue(RGBToHex(values.r, values.g, values.b));
     });
   };
@@ -100,65 +173,6 @@ function getBase64(file) {
   var reader = new FileReader();
   reader.readAsDataURL(file);
   return reader;
-}
-
-async function getAverageRGB(imgBase) {
-  var dimensions = {
-    w: 0,
-    h: 0
-  };
-  dimensions = await getImageDimensions(imgBase);
-  var imgEl = document.createElement('img');
-  imgEl.src = imgBase;
-  var blockSize = 5,
-      defaultRGB = {
-    r: 0,
-    g: 0,
-    b: 0
-  },
-      canvas = document.createElement('canvas'),
-      context = canvas.getContext && canvas.getContext('2d'),
-      data,
-      width,
-      height,
-      i = -4,
-      length,
-      rgb = {
-    r: 0,
-    g: 0,
-    b: 0
-  },
-      count = 0;
-
-  if (!context) {
-    return defaultRGB;
-  }
-
-  height = canvas.height = dimensions.h;
-  width = canvas.width = dimensions.w;
-  context.drawImage(imgEl, 0, 0);
-
-  try {
-    data = context.getImageData(0, 0, width, height);
-  } catch (e) {
-    console.log(e);
-    alert('x');
-    return defaultRGB;
-  }
-
-  length = data.data.length;
-
-  while ((i += blockSize * 4) < length) {
-    ++count;
-    rgb.r += data.data[i];
-    rgb.g += data.data[i + 1];
-    rgb.b += data.data[i + 2];
-  }
-
-  rgb.r = ~~(rgb.r / count);
-  rgb.g = ~~(rgb.g / count);
-  rgb.b = ~~(rgb.b / count);
-  return rgb;
 }
 
 function getImageDimensions(file) {
